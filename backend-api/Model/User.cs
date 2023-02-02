144d3feaa9603b4f5dacad38a1be50131ca4daa0
unknown <khadearjun@gmail.com>
2023-02-01 23:38:54 -0800

    backend validation added!

--- backend-api/Model/User.cs
+++ backend-api/Model/User.cs
@@ -8,11 +8,27 @@ namespace backend_api.Model
     {
         [Key]
        public Guid Id { get; set; }
-       public string Name { get; set; }
-       public string Email { get; set; }
-       public string Phone { get; set; }    
-       public string Topic { get; set; }
-       public string TimePreference { get; set; }
+
+        [Required(ErrorMessage ="Name is Required!")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Email is Required!")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                            ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is Required!")]
+        [RegularExpression(@"^\d{10}$")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Topic is Required!")]
+        public string Topic { get; set; }
+
+        [Required(ErrorMessage = "TimePreference is Required!")]
+        public string TimePreference { get; set; }
 
         [NotMapped]
        public bool Subscription { get; set; }
